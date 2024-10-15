410d4e0e4d9059bbafa218c2aad14e8f622fdc59
dandelion-systems <dandelion.systems@gmail.com>
2024-10-15 11:06:25 +0300

    Change license to MIT

--- src/mediameta/dataroutines.py
+++ src/mediameta/dataroutines.py
@@ -8,19 +8,14 @@
 	2. exif-heic-js (https://github.com/exif-heic-js/exif-heic-js), Copyright (c) 2019 Jim Liu
 
 	mediameta is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+	it under the terms of the MIT License.
 
 	mediameta is distributed in the hope that it will be useful, but
 	WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-	General Public License for more details.
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+	See the MIT License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with mediameta. If not, see <http://www.gnu.org/licenses/>.
-
-	SPDX-License-Identifier: GPL-3.0-or-later
+	SPDX-License-Identifier: MIT
 '''
 from typing import Literal
 from struct import unpack_from
